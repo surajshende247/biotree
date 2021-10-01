300b25059ca8cec70f17a4d64f0a93f0bcf2cf7e
Suraj <surajshende247@gmail.com>
2021-10-01 22:54:18 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+  import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default App;
+  import Avatar from '@mui/material/Avatar';
+  import Stack from '@mui/material/Stack';
+  import Paper from '@mui/material/Paper';
+  import Button from '@mui/material/Button';
+  import Container from '@mui/material/Container';
+
+  import './index.css';   
+
+  function App(){
+
+    let user = '/roadtocode-org';
+    if(window.location.pathname!=="/")
+    {
+      user=window.location.pathname;
+      console.log(user);      
+    }
+    
+
+    const theme = createTheme({
+      palette: {
+        github: {
+          main: '#000000',
+        },
+        linkedin:{
+          main: '#2867B2',
+        },
+        youtube: {
+          main: '#FF0000',
+        },
+        twitter: {
+          main: '#1DA1F2',
+        },  
+        instagram: {
+          main: '#F77737'
+        }
+      },
+    });
+
+    const data =require(`./users${user}.json`);
+    return(
+      <Container maxWidth="md">
+        <ThemeProvider theme={theme}>
+       
+          <Paper 
+          className="user-card"          
+          elevation={12}
+          sx={{
+             p: 2
+            }}>
+
+            <Stack
+            direction="column" 
+            sx={{
+              width: '100%',
+              mx: 'auto', 
+              textAlign: 'center',
+              }}>
+
+            <Avatar
+              alt={data.name}
+              src={`http://github.com${user}.png`}
+              sx={{ width: 100, height: 100, mx: 'auto', textAlign: 'center' }}
+            />
+
+            <h1>{data.name}</h1>
+
+            <p>{data.bio}</p>       
+
+            </Stack>  
+            
+          </Paper>
+        
+              <br /><br />
+        <div >
+          <Stack direction="column" spacing={2}>
+            {
+            data.links.map((link)=>(
+              <Button 
+              variant="contained" 
+              color={link.icon} 
+              style={{color: '#ffffff'}}
+              href={link.url} 
+              target="_blank" >
+                <i className={`fab fa-${link.icon}`}></i>                  
+                  &nbsp;&nbsp;&nbsp;
+                  {link.name}                                           
+              </Button>
+            ))
+            }
+          </Stack>          
+        </div>
+        </ThemeProvider>
+      </Container>
+    );
+  }
+
+  export default App;
